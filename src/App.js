1594051d1a25cbe0fa1ae67a8b29d8be4a12a361
westzerokim <lovetb22@gmail.com>
2023-10-24 17:55:52 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+const Father = styled.div`
+  display: flex;
+  background-color: "red";
+  width: 400;
+  height: 500;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Text = styled.h1`
+  color: white;
+`;
+
+const Circle = styled(Box)`
+  background-color: ${(props) => props.bgColor};
+  border-radius: 50px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Father>
+      <Box bgColor="pink">
+        <Text>
+          hello. Today is not good but everythings, everywhere all at once
+        </Text>
+      </Box>
+      <Box bgColor="tomato" />
+      <Box bgColor="blue" />
+      <Box bgColor="purple" />
+      <Box bgColor="green" />
+      <Circle bgColor="whitesmoke" />
+    </Father>
   );
 }
 
